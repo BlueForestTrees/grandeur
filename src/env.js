31f76aeb7ea4dc2d5ca3c3b9b52e1bf0b7384a4c
slim <smedini@gmail.com>
2018-08-28 19:28:15 +0200

    pas de db pour grandeur

--- src/env.js
+++ src/env.js
@@ -6,14 +6,7 @@ const ENV = {
     PORT: process.env.PORT || 8080,
     
     REST_PATH: process.env.REST_PATH || "rest",
-    
-    DB_NAME: process.env.DB_NAME || "BlueForestTreesDB",
-    DB_HOST: process.env.DB_HOST || "localhost",
-    DB_PORT: process.env.DB_PORT || 27017,
-    DB_USER: process.env.DB_USER || "doudou",
-    DB_PWD: process.env.DB_PWD || "masta",
-    DB_COLLECTION: process.env.DB_COLLECTION || "grandeur",
-    
+
     NODE_ENV: process.env.NODE_ENV || null,
     VERSION: version,
     MORGAN: process.env.MORGAN || ':status :method :url :response-time ms - :res[content-length]',
